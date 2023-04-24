5783ea610d9d58e2a52a24b0eae807dab4e2e973
PradheshKumar <pradheshkumar29@gmail.com>
2023-04-24 22:36:04 +0530

    Updated Responsiveness

--- gdsc/src/App.js
+++ gdsc/src/App.js
@@ -1,5 +1,3 @@
-import logo from "./logo.svg";
-import "./App.css";
 import DomainSplitPage from "./Pages/DomainSplit/DomainSplitPage";
 import Navbar from "./components/Navbar/Navbar";
 import HeroPage from "./Pages/Hero/HeroPage";
