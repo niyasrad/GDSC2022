8b1d18599670e30e2229cf92b26bcc974193582a
QuadraFour <pradosh.balaji@gmail.com>
2023-04-25 00:42:22 +0530

    Update App.js

--- gdsc/src/App.js
+++ gdsc/src/App.js
@@ -1,6 +1,8 @@
 import DomainSplitPage from "./Pages/DomainSplit/DomainSplitPage";
 import Navbar from "./components/Navbar/Navbar";
 import HeroPage from "./Pages/Hero/HeroPage";
+import EventInfoPage from "./Pages/EventInfo/EventInfoPage";
+import MarketingPage from "./Pages/Marketing/marketingPage";
 function App() {
   return (
     <>
