fb5daa94c049bb9df7c7756289622849b55f750c
QuadraFour <pradosh.balaji@gmail.com>
2023-04-25 00:31:22 +0530

    Completed EventInfo page

--- gdsc/src/App.js
+++ gdsc/src/App.js
@@ -1,11 +1,13 @@
 import DomainSplitPage from "./Pages/DomainSplit/DomainSplitPage";
 import Navbar from "./components/Navbar/Navbar";
 import HeroPage from "./Pages/Hero/HeroPage";
+import EventInfoPage from "./Pages/EventInfo/EventInfoPage";
 function App() {
   return (
     <>
       <Navbar />
       <HeroPage />
+      <EventInfoPage />
       <DomainSplitPage />
     </>
   );
