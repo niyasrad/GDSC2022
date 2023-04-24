29067d8bdb81fcf6eed95c7770c5d10a0ec707ed
PradheshKumar <pradheshkumar29@gmail.com>
2023-04-24 22:01:36 +0530

    Initial Marketing Page

---
+++ gdsc/src/components/marketing.js
@@ -0,0 +1,26 @@
+import "./marketing.css";
+
+function MarketingPage() {
+  return (
+    <div className="marketing-body">
+      <div className="marketing-header">Marketing and Pitching ⚡️</div>
+      <div className="cap-icon">
+        <img src="./assets/image6.svg" />
+      </div>
+      <div className="marketing-content">
+        <img src="./assets/marketing-image.svg"></img>
+        <div className="marketing-description">
+          At ProductForge, we understand that building a great product is only
+          half the battle. That's why we also focus on teaching you how to
+          market and pitch your product to potential customers and investors.
+          Learn how to create compelling pitches, conduct market research, and
+          develop effective marketing strategies. You'll get the chance to
+          showcase your skills in front of a panel of experts and compete for
+          prizes.
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MarketingPage;
