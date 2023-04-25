e7d6019f310a03de038b6e3eb3422c374495d633
Shelter <pradosh.balaji@gmail.com>
2023-04-25 19:21:57 +0530

    Improved Navbar and Hero page

--- gdsc/src/components/Navbar/Navbar.js
+++ gdsc/src/components/Navbar/Navbar.js
@@ -37,16 +37,13 @@ function Navbar() {
       <div>
         {toggleMenu || screenWidth > 860 ? (
           <ul className="nav-elements">
-            {toggleMenu && screenWidth < 860 ? (
-              <i className="fa-solid fa-times" onClick={toggleNav}></i>
-            ) : null}
             <li>
-              <a href="/" className="nav-link">
+              <a href="#info" className="nav-link">
                 About
               </a>
             </li>
             <li>
-              <a href="#about" className="nav-link">
+              <a href="#info" className="nav-link">
                 Schedule
               </a>
             </li>
@@ -63,12 +60,21 @@ function Navbar() {
           </ul>
         ) : (
           <div className="sidebar-toggle">
-            <i className="fa-solid fa-bars" onClick={toggleSidebar}></i>
+            {showSidebar ? (
+              <div></div>
+            ) : (
+              <i className="fa-solid fa-bars" onClick={toggleSidebar}></i>
+            )}
           </div>
         )}
       </div>
       <div className={`${showSidebar ? "mobile-nav__wrapper" : "hide"}`}>
         <div className={`sidebar ${showSidebar ? "sidebar-open" : ""}`}>
+          <div className="sidebar-header">
+            {showSidebar && (
+              <i className="fa-solid fa-times fa-lg" onClick={toggleSidebar}></i>
+            )}
+          </div>
           <ul className="sidebar-nav-elements">
             <li>
               <a href="/" className="nav-link">
