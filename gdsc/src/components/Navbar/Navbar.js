ae5821d577148aafc7bcf3cdbee2243c52f69741
QuadraFour <pradosh.balaji@gmail.com>
2023-04-25 13:27:34 +0530

    Navbar fix

--- gdsc/src/components/Navbar/Navbar.js
+++ gdsc/src/components/Navbar/Navbar.js
@@ -2,25 +2,32 @@ import "./Navbar.css";
 import logo from "../../assets/logo.svg";
 import Button from "../Button/Button";
 import { useState, useEffect } from "react";
+
 function Navbar() {
   const [toggleMenu, setToggleMenu] = useState(false);
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [showSidebar, setShowSidebar] = useState(false);
+
   const toggleNav = () => {
     setToggleMenu(!toggleMenu);
   };
 
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
   useEffect(() => {
     const changeWidth = () => {
-        setScreenWidth(window.innerWidth);
+      setScreenWidth(window.innerWidth);
     };
     window.addEventListener("resize", changeWidth);
     return () => {
-        window.removeEventListener("resize", changeWidth);
+      window.removeEventListener("resize", changeWidth);
     };
-    }, []);
+  }, []);
 
   return (
-    <nav>
+    <div className="navbar-wrapper">
       <div className="logo">
         <img src={logo} alt="logo" />
         <h4 className="title">
@@ -53,13 +60,40 @@ function Navbar() {
                 <Button>Register</Button>
               </a>
             </li>
-           
           </ul>
         ) : (
-          <i className="fa-solid fa-bars" onClick={toggleNav}></i>
+          <div className="sidebar-toggle">
+            <i className="fa-solid fa-bars" onClick={toggleSidebar}></i>
+          </div>
         )}
       </div>
-    </nav>
+      <div className={`${showSidebar ? "mobile-nav__wrapper" : "hide"}`}>
+        <div className={`sidebar ${showSidebar ? "sidebar-open" : ""}`}>
+          <ul className="sidebar-nav-elements">
+            <li>
+              <a href="/" className="nav-link">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#about" className="nav-link">
+                Schedule
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="nav-link">
+                Contact
+              </a>
+            </li>
+            <li>
+              <a href="#register">
+                <Button>Register</Button>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
 
