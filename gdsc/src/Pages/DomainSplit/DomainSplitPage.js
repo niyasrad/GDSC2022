e5ca749e90fa563503bd133c88a53de3807fbd3f
Niyas Hameed <niyasrad@gmail.com>
2023-04-26 19:35:38 +0530

    [Niyas][Issue] Fixed Issue #14 (#16)
    Co-authored-by: Niyas Hameed <niyas@cirqt.com>

--- gdsc/src/Pages/DomainSplit/DomainSplitPage.js
+++ gdsc/src/Pages/DomainSplit/DomainSplitPage.js
@@ -6,20 +6,22 @@ import CLOUDimg from "../../assets/Cloud.svg";
 function DomainSplitPage() {
   return (
     <div className="domain-body" id='domains'>
-      <div className="domain-heading">All Domains of Technology</div>
-      <div className="domain-icons">
-        <img className="domain-web" src={WEBmg} alt="Web" />
-        <img className="domain-ml" src={MLimg} alt="ML" />
-        <img className="domain-uiux" src={UIUXimg} alt="UIUX" />
-        <img className="domain-cloud" src={CLOUDimg} alt="Cloud" />
-      </div>
-      <div className="domain-text">
-        Discover the latest and most impactful technologies across all domains,
-        including web, cloud, machine learning, and UI. Our experienced
-        instructors will provide hands-on workshops that will take you through
-        the complete development lifecycle of a product. You'll get exposure to
-        industry-leading tools and methodologies, and learn best practices for
-        building products that scale.
+      <div className="domain-body__content">
+        <h1 className="domain-heading">All Domains of Technology</h1>
+        <div className="domain-icons">
+          <img className="domain-web" src={WEBmg} alt="Web" />
+          <img className="domain-ml" src={MLimg} alt="ML" />
+          <img className="domain-uiux" src={UIUXimg} alt="UIUX" />
+          <img className="domain-cloud" src={CLOUDimg} alt="Cloud" />
+        </div>
+        <div className="domain-text">
+          Discover the latest and most impactful technologies across all domains,
+          including web, cloud, machine learning, and UI. Our experienced
+          instructors will provide hands-on workshops that will take you through
+          the complete development lifecycle of a product. You'll get exposure to
+          industry-leading tools and methodologies, and learn best practices for
+          building products that scale.
+        </div>
       </div>
     </div>
   );
