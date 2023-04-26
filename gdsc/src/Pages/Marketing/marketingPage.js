3b4b17cf80dd2f25c4593a0906c072835d1d4122
Niyas Hameed <niyas@cirqt.com>
2023-04-26 19:22:41 +0530

    [Niyas][Issue] Fixed Issue #14

--- gdsc/src/Pages/Marketing/marketingPage.js
+++ gdsc/src/Pages/Marketing/marketingPage.js
@@ -4,20 +4,22 @@ import marketingImage from "../../assets/marketing-image.svg";
 function MarketingPage() {
   return (
     <div className="marketing-body">
-      <div className="marketing-header">Marketing and Pitching ⚡️</div>
-      <div className="cap-icon">
-        <img src={hatImage} alt="Hat" />
-      </div>
-      <div className="marketing-content">
-        <img src={marketingImage} alt="Marketing"></img>
-        <div className="marketing-description">
-          At ProductForge, we understand that building a great product is only
-          half the battle. That's why we also focus on teaching you how to
-          market and pitch your product to potential customers and investors.
-          Learn how to create compelling pitches, conduct market research, and
-          develop effective marketing strategies. You'll get the chance to
-          showcase your skills in front of a panel of experts and compete for
-          prizes.
+      <div className="marketing-body__content">
+        <div className="marketing-header">Marketing and Pitching ⚡️</div>
+        <div className="cap-icon">
+          <img src={hatImage} alt="Hat" />
+        </div>
+        <div className="marketing-content">
+          <img src={marketingImage} alt="Marketing"></img>
+          <div className="marketing-description">
+            At ProductForge, we understand that building a great product is only
+            half the battle. That's why we also focus on teaching you how to
+            market and pitch your product to potential customers and investors.
+            Learn how to create compelling pitches, conduct market research, and
+            develop effective marketing strategies. You'll get the chance to
+            showcase your skills in front of a panel of experts and compete for
+            prizes.
+          </div>
         </div>
       </div>
     </div>
